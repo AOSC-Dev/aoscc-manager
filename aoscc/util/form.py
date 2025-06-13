41868376d6cbbf5fdfbda8933c1753f1a628d30e
TechCiel <i@ciel.dev>
2025-06-13 18:26:42 -0400

    feat(form): allow empty to pass cond, add form dict arg, remove my_flash

--- aoscc/util/form.py
+++ aoscc/util/form.py
@@ -7,13 +7,11 @@ from flask import request, flash
 Field = namedtuple('Field', 'display,name,minlen,maxlen,factory,condition')
 
 
-def validate(*fields: Field, show_flash: bool = True) -> dict | None:
-    def my_flash(msg: str):
-        if show_flash:  # flash suppressor
-            flash(msg)
+def validate(*fields: Field, form: dict = None) -> dict | None:
+    form = form or request.form
     ret = {}
     for field in fields:
-        val = request.form.get(field.name, '').strip()
+        val = form.get(field.name, '').strip()
         try:
             # 1. string length check
             if not field.minlen <= len(val) <= field.maxlen:
@@ -22,7 +20,7 @@ def validate(*fields: Field, show_flash: bool = True) -> dict | None:
             typed_val = field.factory(val)
             # 3. build cond func
             cond = field.condition
-            if cond is True:
+            if not typed_val or cond is True:
                 cond = lambda _: True
             if isinstance(cond, str):
                 cond = lambda x, pattern=cond: bool(re.fullmatch(pattern, x))
@@ -33,12 +31,12 @@ def validate(*fields: Field, show_flash: bool = True) -> dict | None:
             ret[field.name] = typed_val
         except Exception:
             if field.display.endswith('！'):
-                my_flash(field.display)  # customized error msg
+                flash(field.display)  # customized error msg
             else:
-                my_flash(f'{field.display}格式错误！')
+                flash(f'{field.display}格式错误！')
             return
     if not ret:
-        my_flash('无效表单。')
+        flash('无效表单。')
         return
     return ret
 
