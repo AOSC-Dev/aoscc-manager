5c9aaac84c4886a793b59116684c9741607de6cd
TechCiel <i@ciel.dev>
2025-06-25 08:11:35 -0400

    revert: 7f62c5c partially

--- aoscc/user/__init__.py
+++ aoscc/user/__init__.py
@@ -10,8 +10,8 @@ def user_check():
     if not g.uid:  # all pages require logged in
         return redirect(url_for('login.login'))
     g.register = fetch_one('register', {'uid': g.uid})
-    g.registered = bool(g.register and (g.register['arrived'] >= 0))
-    g.arrived = (g.register['arrived'] > 0) if g.register else False
+    g.registered = bool(g.register)
+    g.arrived = bool(g.register and g.register['arrived'])
     if not g.nick and request.endpoint not in ('user.info', 'user.post_info'):
         return redirect(url_for('user.info'))  # provide nick before other service
 
